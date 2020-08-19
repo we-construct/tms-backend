8099ba8def333750eef42774f41bab06d49a804e
Vlad <vaarsenyan@gmail.com>
2020-08-19 21:01:01 +0400

    Added authorisation route and functional with some validation

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ require("dotenv").config();
 
 const port = process.env.PORT || 5000;
 const invitationRouter = require("./Routes/invitation");
+const authorizationRouter = require("./Routes/authorization");
 // initializing express
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use("/accept-initation", invitationRouter);
+app.use("/login", authorizationRouter);
 
 app.listen(port, () => {
   console.log("Server run on port " + port);
