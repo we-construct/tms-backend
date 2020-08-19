2f50985657a884a79d5ad32c07eb0925d6c99172
Vlad <vaarsenyan@gmail.com>
2020-08-19 19:36:05 +0400

    Accepting invitation / some validations, crypting password / saving in db

--- server.js
+++ server.js
@@ -1,29 +1,15 @@
 const express = require("express");
 const cors = require("cors");
-const pgp = require("pg-promise")(/*options*/);
 require("dotenv").config();
 
-const { PG_USER, PG_HOST, PG_PASSWORD, PG_PORT, PG_DB } = process.env;
 const port = process.env.PORT || 5000;
-
-// connecting db
-const db = pgp(
-  `${PG_USER}://${PG_USER}:${PG_PASSWORD}@${PG_HOST}:${PG_PORT}/${PG_DB}`
-);
-
+const invitationRouter = require("./Routes/invitation");
 // initializing express
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
-db.query("select * from statuses")
-  .then(function (data) {
-    console.log("DATA:", data);
-  })
-  .catch(function (error) {
-    console.log("ERROR:", error);
-  });
+app.use("/", invitationRouter);
 
 app.listen(port, () => {
   console.log("Server run on port " + port);
