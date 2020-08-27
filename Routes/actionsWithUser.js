69f527073b918368fba0b83e3b1b470594a42b1d
Vlad <vaarsenyan@gmail.com>
2020-08-27 15:06:04 +0400

    User edit route

--- Routes/actionsWithUser.js
+++ Routes/actionsWithUser.js
@@ -24,5 +24,14 @@ router.route("/set-status").post(authenticateAdmin, async (req, res) => {
     res.json("Something goes wrong :(");
   }
 });
+router.route("/update-user").post(authenticateAdmin, async (req, res) => {
+  console.log(req.body);
+  try {
+      await db.query(`update users set first_name = '${req.body.firstName}', last_name = '${req.body.lastName}', email = '${req.body.email}', phone_number = '${req.body.phoneNumber}', position_id = '${req.body.positionId}', role_id = '${req.body.roleId}', status_id = '${req.body.statusId}', updated_at = to_timestamp(${Date.now()} / 1000.0) where id = ${req.body.id}`);
+      res.json({ message: "User data updated" });
+  } catch (error) {
+    res.json(error.message);
+  }
+});
 
 module.exports = router;
